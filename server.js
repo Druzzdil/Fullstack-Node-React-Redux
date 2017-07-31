6bf50196645a6abc3d9051b3a963bd0bec30d80a
Druzzdil <staniszewski86@gmail.com>
2017-07-31 17:20:42 +0200

    refactor structure

--- server.js
+++ server.js
@@ -1,36 +1,11 @@
-
-const passport = require('passport')
-
-const keys = require('./config/keys');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const express = require('express')
+require('./services/passport');
+
 const app = express();
 
-//passport part
-passport.use(
-  new GoogleStrategy(
-    {
-      clientID: keys.googleClientID,
-      clientSecret: keys.googleClientSecret,
-      callbackURL: '/auth/google/callback'
-    },(accessToken, refreshToken, profile, done)=>{
-        console.log('accesToken', accessToken)
-        console.log('refreshToken ',refreshToken)
-        console.log('profile', profile)
-    }
-  ));
+require('./routes/authRoutes')(app);
 
-
-app.get('/auth/google', passport.authenticate ('google', {
-      scope: ['profile', 'email']
-   })
-)
-
-// secent route for callback redirection from passport auth
-app.get('/auth/google/callback', passport.authenticate('google'));
-//passport part
-
-
+//import routes from routes folder
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(port, 'using port 3000')
