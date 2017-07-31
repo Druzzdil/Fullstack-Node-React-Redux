0d4b8d9ba9c98613406216c3ab611c03c7262bf9
Druzzdil <staniszewski86@gmail.com>
2017-07-31 16:48:41 +0200

    handling server callback

--- server.js
+++ server.js
@@ -6,14 +6,17 @@ const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const express = require('express')
 const app = express();
 
+//passport part
 passport.use(
   new GoogleStrategy(
     {
       clientID: keys.googleClientID,
       clientSecret: keys.googleClientSecret,
       callbackURL: '/auth/google/callback'
-    },(accessToken)=>{
-      console.log(accessToken)
+    },(accessToken, refreshToken, profile, done)=>{
+        console.log('accesToken', accessToken)
+        console.log('refreshToken ',refreshToken)
+        console.log('profile', profile)
     }
   ));
 
@@ -25,6 +28,8 @@ app.get('/auth/google', passport.authenticate ('google', {
 
 // secent route for callback redirection from passport auth
 app.get('/auth/google/callback', passport.authenticate('google'));
+//passport part
+
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
