8e5572efddd4faba28fa09246fb04e8ba7e136f7
Druzzdil <staniszewski86@gmail.com>
2017-07-31 12:44:24 +0200

    progress with oauth

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 
 const passport = require('passport')
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 const express = require('express')
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const app = express();
+
+
+// const clientID = '1090551703725-1dqnjkb33sf0fnsf171r3k4a7hpfhjt7.apps.googleusercontent.com'
+// const clientSecret = 'm5XzJGbdsYf1KZugEZVdrOt-'
 
 
 passport.use(new GoogleStrategy());
