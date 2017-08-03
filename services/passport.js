d5988fb753bcf071d78ba9d2dd085d086c9531fd
Druzzdil <staniszewski86@gmail.com>
2017-08-03 14:15:10 +0200

    create deserialize function

--- services/passport.js
+++ services/passport.js
@@ -11,13 +11,14 @@ passport.serializeUser((user, done)=>{
     done(null, user.id);
 })
 
-//resposible for generating cookie, id it's from mongo id record
+// resposible for generating cookie, id it's from mongo id record
 
-// passport.deserializeUser((id, done) => {
-//   User.findById(id, (err, user)=> {
-//     done(err, user);
-//   });
-// });
+passport.deserializeUser((id, done) => {
+  User.findById(id).then(user =>{
+      done(null, user)
+  });
+});
+
 
 
 passport.use(
