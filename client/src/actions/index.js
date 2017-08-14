eedd5bac03d8f8a6511cb964b61fb112034fd54d
Druzzdil <staniszewski86@gmail.com>
2017-08-14 16:44:08 +0200

    create action creator - dispach function type: payload:

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,7 +1,11 @@
 import axios from 'axios'
-import {FETCH_USER} from 'types'
+import {FETCH_USER} from './types'
 
 
 const fetchUser = () => {
-    axios.get('api/user')
+    return dispatch => {
+        axios
+            .get('api/user')
+            .then(res => dispatch({type: FETCH_USER, payload: res}))
+    }
 }
