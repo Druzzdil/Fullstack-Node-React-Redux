89a32ae176a4b85022082fa92f63b8772bb8e1db
Druzzdil <staniszewski86@gmail.com>
2017-08-14 13:47:05 +0200

    create reducers and add it to store in index.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
+import {Provider} from 'react-redux';
+import {createStore, applyMiddleware} from 'redux';
+import reducers from './reducers';
 
 
+const store = createStore(reducers, {}, applyMiddleware() )
+//this is basic store with empty reducer
 
 
-ReactDOM.render(<App/>, document.getElementById('root'))
+//provider is from redux, its know about states updates
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>
+    , document.getElementById('root'))
