8b18c2ca495396e6d01c4c1c0e24a52dc8f4843e
Druzzdil <staniszewski86@gmail.com>
2017-08-14 14:03:20 +0200

    add first basic route component

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,8 +1,24 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+
+
+const Header = () => <div>header</div>
+const Dashboard = () => <div>Dashboard</div>
+const SurveyNew = () => <div>SurveyNew</div>
+const Landing = () => <div>Landing</div>
+
+
 
 const App = () => {
     return (
-        <div><h1>appcomp</h1></div>
+        <div>
+            <BrowserRouter>
+                <div>
+                    <Route path ="/" component={Landing}/>
+                </div>
+            </BrowserRouter>
+        </div>
     )
 }   
 
