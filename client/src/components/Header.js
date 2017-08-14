138cb0ffc4f62e24171f91688e73d67974ea5e87
Druzzdil <staniszewski86@gmail.com>
2017-08-14 15:25:42 +0200

    add simple header styling from materialize.css

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -6,16 +6,14 @@ import React, { Component } from 'react';
     super(props);
 
   }
-  
+
   render() {
     return (
       <nav>
         <div className="nav-wrapper">
-          <a href="#" className="brand-logo">Logo</a>
+          <a href="#" className="brand-logo">Emaily</a>
           <ul id="nav-mobile" className="right hide-on-med-and-down">
-            <li><a href="sass.html">Sass</a></li>
-            <li><a href="badges.html">Components</a></li>
-            <li><a href="collapsible.html">JavaScript</a></li>
+            <li><a href="sass.html">Login with Google</a></li>
           </ul>
         </div>
       </nav>
