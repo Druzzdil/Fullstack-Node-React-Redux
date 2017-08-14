4d0829097f3735ebd505366d1c930c5722cc6839
Druzzdil <staniszewski86@gmail.com>
2017-08-14 16:57:55 +0200

    change functional app component to class component, wire up an action creator in componenndidmount method

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,26 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 import Header from './Header';
 
 const Dashboard = () => <div>Dashboard</div>
 const SurveyNew = () => <div>SurveyNew</div>
-const Landing = () => <div>Landing</div>
+const Landing = () => <div><h1>Landing</h1></div>
 
 
 
-const App = () => {
-    return (
-        <div>
-            <BrowserRouter>
-                <div className="container">
-                    <Header />
-                    <Route  exact path ="/" component={Landing}/>
-                    <Route  exact path ="/surveys" component={Dashboard}/>
-                    <Route  path ="/surveys/new" component={SurveyNew}/>
-                </div>
-            </BrowserRouter>
-        </div>
-    )
-}   
+class App extends Component {
+
+    constructor(props){
+        super(props);
+
+    }
+
+    componentDidMount(){
+
+    }
+    
+    render() {
+        return (
+            <div>
+                <BrowserRouter>
+                    <div className="container">
+                        <Header />
+                        <Route  exact path ="/" component={Landing}/>
+                        <Route  exact path ="/surveys" component={Dashboard}/>
+                        <Route  path ="/surveys/new" component={SurveyNew}/>
+                    </div>
+                </BrowserRouter>
+            </div>
+        )
+    }   
+}
 
 export default App;
